2638a7d9d9fb9113072b95451d9427b95c5a05ec
Namratha98 <42108024+Namrathalb@users.noreply.github.com>
2020-02-27 11:45:23 +0530

    Create GPS.py
    Code to check the working of GPS module

---
+++ GPS Module/Approch-1/GPS.py
@@ -0,0 +1,23 @@
+
+#read the Readme file for installing dependencies and libraries
+# Raspberry pi 4 with Neo6m GPS Module
+
+import serial
+import time
+import string
+import pynmea2
+import datetime
+
+while True:
+    port="/dev/ttyAMA0"
+    ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata=ser.readline()
+
+    if newdata[0:6] == "$GPRMC":
+        newmsg=pynmea2.parse(newdata)
+        lat=newmsg.latitude
+        lng=newmsg.longitude
+        gps = "Latitude=" + str(lat) + "\nLongitude=" + str(lng)
+        print(gps)
+        print datetime.datetime.now()
