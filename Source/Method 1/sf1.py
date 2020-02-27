dc5aa69bcac0ae419b3c9e705e9c3b172c147752
Namratha98 <42108024+Namrathalb@users.noreply.github.com>
2020-02-27 12:27:16 +0530

    Create sf1.py

---
+++ Source/Method 1/sf1.py
@@ -0,0 +1,106 @@
+import smbus
+import os
+import serial
+import time
+import string
+import csv                                                                           
+import pynmea2
+import datetime #import SMBus module of I2C
+from time import sleep          #import
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+
+def MPU_Init():
+    #write to sample rate register
+    bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+    
+    #Write to power management register
+    bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+    
+    #Write to Configuration register
+    bus.write_byte_data(Device_Address, CONFIG, 0)
+    
+    #Write to Gyro configuration register
+    bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+    
+    #Write to interrupt enable register
+    bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+def read_raw_data(addr):
+    #Accelero and Gyro value are 16-bit
+        high = bus.read_byte_data(Device_Address, addr)
+        low = bus.read_byte_data(Device_Address, addr+1)
+    
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+        
+        #to get signed value from mpu6050
+        if(value > 32768):
+                value = value - 65536
+        return value
+
+
+bus = smbus.SMBus(1)    # or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+MPU_Init()
+
+print ("Reading Data of GPS and Accelerometer")
+file=open('/home/pi/Desktop/saferide/sources/Method 1 with gps/sensor_readings.csv',mode='a')
+if os.stat("/home/pi/Desktop/saferide/sources/Method 1 with gps/sensor_readings.csv").st_size == 0:
+            file.write("x-axis, y-axis, z-axis, Latitude, Longitude, Timestamp\n")
+while True:
+    
+    port="/dev/ttyAMA0"
+    ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata=ser.readline()
+    
+    #Read Accelerometer raw value
+    acc_x = read_raw_data(ACCEL_XOUT_H)
+    acc_y = read_raw_data(ACCEL_YOUT_H)
+    acc_z = read_raw_data(ACCEL_ZOUT_H)
+    
+    #Read Gyroscope raw value
+    #gyro_x = read_raw_data(GYRO_XOUT_H)
+    #gyro_y = read_raw_data(GYRO_YOUT_H)
+    #gyro_z = read_raw_data(GYRO_ZOUT_H)
+    
+    #Full scale range +/- 250 degree/C as per sensitivity scale factor
+    Ax = acc_x/16384.0
+    Ay = acc_y/16384.0
+    Az = acc_z/16384.0
+    
+   #Gx = gyro_x/131.0
+   #Gy = gyro_y/131.0
+   #Gz = gyro_z/131.0
+    
+    if newdata[0:6] == "$GPRMC":
+        newmsg=pynmea2.parse(newdata)
+        lat=newmsg.latitude
+        lng=newmsg.longitude
+        gps = "Latitude=" + str(lat) + "\nLongitude=" + str(lng)
+        timestamp = datetime.datetime.now()
+        print "Ax = %.2f" %Ax,   "Ay=%.2f" %Ay,   "Az=%.2f" %Az
+        print(gps)
+        print timestamp
+        print
+        
+        file.write(str(Ax)+","+str(Ay)+","+str(Az)+","+str(lat)+","+str(lng)+","+str(timestamp)+"\n")
+        file.flush()
+        
+        sleep(1)
+        
+
